b9720cd00ecd846e169aec51e08ec45e02e9f1e3
Fahim <fahimshahariar.fs@gmail.com>
2024-09-02 10:25:12 +0600

    Add project files.

---
+++ Auctions/Models/Listing.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Auctions.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public double price { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public bool IsSold { get; set; }
+
+        [Required]
+        public string? IndentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser? User { get; set; }
+
+
+        public List<Bid>? Bids { get; set; }
+        public List<Comment>? Comments { get; set; }
+
+
+    }
+}
